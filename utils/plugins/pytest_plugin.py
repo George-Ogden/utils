21a15fa886f2c061d2e0cb42c39adfca14b83d9b
George Ogden <george-ogden@outlook.com>
2026-03-29 12:08:42 +0100

    Define fixture to setup debug

--- utils/plugins/pytest_plugin.py
+++ utils/plugins/pytest_plugin.py
@@ -3,6 +3,17 @@ from pathlib import Path
 import pytest
 
 
+@pytest.fixture(autouse=True, scope="session")
+def setup_debug() -> None:
+    try:
+        from debug import CONFIG, install
+    except ImportError:
+        ...
+    else:
+        install()
+        CONFIG.sort_unordered_collections = True
+
+
 @pytest.fixture
 def filepath(filename: str) -> Path:
     return Path(filename)
--- utils/plugins/pytest_plugin_test.py
+++ utils/plugins/pytest_plugin_test.py
@@ -1,12 +1,20 @@
+import builtins
 import os
 from pathlib import Path
 
+from debug import CONFIG
 import pytest
 
 from .pytest_plugin import filepath as filepath
+from .pytest_plugin import setup_debug as setup_debug
 
 
 @pytest.mark.parametrize("filename", ["filename.py", "thispathdoesnotexist.txt", "nested/folder"])
 def test_filename_to_filepath(filename: str, filepath: Path) -> None:
     assert filename == os.fspath(filepath)
     assert isinstance(filepath, Path)
+
+
+def test_debug() -> None:
+    assert "dbg" in builtins.__dict__
+    assert CONFIG.sort_unordered_collections
