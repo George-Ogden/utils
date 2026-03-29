82fcb6678ea7675292a6ef869413d559ce94f097
George Ogden <george-ogden@outlook.com>
2026-03-29 10:45:53 +0100

    Update check for errors to expect None from the nullcontext

--- utils/test_utils/check_for_errors_test.py
+++ utils/test_utils/check_for_errors_test.py
@@ -16,7 +16,9 @@ def test_check_for_errors_no_error() -> None:
 def test_check_for_errors_correct_error() -> None:
     with check_for_errors(ValueError) as e:
         assert_type(e, pytest.ExceptionInfo[ValueError])
+        assert isinstance(e, pytest.ExceptionInfo)
         float("abc")
+    assert isinstance(e.value, ValueError)
 
 
 @pytest.mark.xfail(strict=True, raises=ValueError)
@@ -30,6 +32,7 @@ def test_check_for_errors_incorrect_error() -> None:
 def test_check_for_errors_all_fine() -> None:
     with check_for_errors(None) as e:
         assert_type(e, pytest.ExceptionInfo[BaseException] | None)
+        assert e is None
         assert True
 
 
@@ -37,6 +40,7 @@ def test_check_for_errors_all_fine() -> None:
 def test_check_for_errors_not_fine() -> None:
     with check_for_errors(None) as e:
         assert_type(e, pytest.ExceptionInfo[BaseException] | None)
+        assert e is None
         raise AssertionError()
 
 
@@ -51,13 +55,16 @@ def test_check_for_assertion_errors_no_error() -> None:
 def test_check_for_assertion_errors_assertion_error() -> None:
     with check_for_assertion_errors(None) as e:
         assert_type(e, pytest.ExceptionInfo[AssertionError])
+        assert isinstance(e, pytest.ExceptionInfo)
         raise AssertionError()
+    assert isinstance(e.value, AssertionError)
 
 
 @pytest.mark.typed
 def test_check_for_assertion_errors_all_fine() -> None:
     with check_for_assertion_errors(1) as e:
         assert_type(e, pytest.ExceptionInfo[AssertionError] | None)
+        assert e is None
         assert True
 
 
