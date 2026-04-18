0d94182fcee4f13edf4c61df7ea5db2ae4403a4c
George Ogden <george-ogden@outlook.com>
2026-04-18 14:57:50 +0100

    Add documentation for new error functions

--- utils/macros.py
+++ utils/macros.py
@@ -4,8 +4,16 @@ from .errors import NotImplementedYetError
 
 
 def unimplemented(*args: object) -> NoReturn:
+    """
+    Raises a `NotImplemented` error.
+    Any additional args will be passed into the error.
+    """
     raise NotImplementedError(*args)
 
 
 def todo(*args: object) -> NoReturn:
+    """
+    Raises a `NotImplementedYet` error, which is a subclass of a `NotImplementedError`.
+    Any additional args will be passed into the error.
+    """
     raise NotImplementedYetError(*args)
